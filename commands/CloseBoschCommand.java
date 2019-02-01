edc2971ca88e103c14d59b0bf032581c5f55005e
Jan Hofman <hofmanjan.00@gmail.com>
2019-02-01 14:23:55 +0100

    Bosch - limit switch (#15)
    * Bosch motor commands using limit switches
    
    * Fixed bug

--- commands/CloseBoschCommand.java
+++ commands/CloseBoschCommand.java
@@ -8,11 +8,14 @@
 package frc.robot.commands;
 
 import edu.wpi.first.wpilibj.command.Command;
+import edu.wpi.first.wpilibj.DigitalInput;
 import frc.robot.Constants;
 import frc.robot.Robot;
 import frc.robot.RobotMap;
 
 public class CloseBoschCommand extends Command {
+
+  DigitalInput closedLimitSwitch = new DigitalInput(5);
 
   //Boolean for checking whether the command is finished -> terminating the command
   private boolean isFinished;
@@ -31,31 +34,29 @@ public class CloseBoschCommand extends Command {
   // Called repeatedly when this Command is scheduled to run
   @Override
   protected void execute() {
-    if(Robot.bosch.getPosition() >= Constants.boschUpperBound) {
-      while(Robot.bosch.getPosition() > Constants.boschLowerBound) {
-        Robot.bosch.setPosition(Robot.bosch.getPosition() - Robot.bosch.counter.get() / 10000);
-        RobotMap.boschMotor.set(-0.5);
-      }
+    if (closedLimitSwitch.get() == true) {
+      RobotMap.boschMotor.set(Constants.boschMotorSpeed);
+    } else {
       RobotMap.boschMotor.set(0);
-      Robot.bosch.counter.reset();
     }
-    isFinished = true;
   }
 
   // Make this return true when this Command no longer needs to run execute()
   @Override
   protected boolean isFinished() {
-    return isFinished;
+    return !closedLimitSwitch.get();
   }
 
   // Called once after isFinished returns true
   @Override
   protected void end() {
+    RobotMap.boschMotor.set(0);
   }
 
   // Called when another command which requires one or more of the same
   // subsystems is scheduled to run
   @Override
   protected void interrupted() {
+    RobotMap.boschMotor.set(0);
   }
 }
