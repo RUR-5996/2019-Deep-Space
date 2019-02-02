131fda51d698eccf9c86d8f906fe2c40ebde2212
Jan Hofman <hofmanjan.00@gmail.com>
2019-02-02 19:31:55 +0100

    Vision

--- subsystems/ShooterSubsystem.java
+++ subsystems/ShooterSubsystem.java
@@ -7,7 +7,10 @@
 
 package frc.robot.subsystems;
 
+import com.ctre.phoenix.motorcontrol.FollowerType;
+
 import edu.wpi.first.wpilibj.command.Subsystem;
+import frc.robot.RobotMap;
 
 /**
  * Add your docs here.
@@ -16,9 +19,29 @@ public class ShooterSubsystem extends Subsystem {
   // Put methods for controlling this subsystem
   // here. Call these from Commands.
 
+  public void shooterInit() {
+    RobotMap.shooter2.follow(RobotMap.shooter1, FollowerType.PercentOutput);
+    RobotMap.shooter4.follow(RobotMap.shooter3, FollowerType.PercentOutput);
+  }
+
+  public void shoot(double speed) {
+    RobotMap.shooter1.set(speed);
+    RobotMap.shooter3.set(speed);
+  }
+
+  public void intake(double speed) {
+    //RobotMap.shooter1.set(speed);
+    //RobotMap.shooter3.set(speed);
+  }
+
+  public void stop(){
+    RobotMap.shooter1.set(0);
+    RobotMap.shooter3.set(0);
+  }
+
   @Override
   public void initDefaultCommand() {
     // Set the default command for a subsystem here.
-    // setDefaultCommand(new MySpecialCommand());
+    //setDefaultCommand(new ShootCommand());
   }
 }
--- subsystems/VisionSubsystem.java
+++ subsystems/VisionSubsystem.java
@@ -9,24 +9,55 @@ package frc.robot.subsystems;
 
 import edu.wpi.first.networktables.NetworkTable;
 import edu.wpi.first.networktables.NetworkTableInstance;
-import edu.wpi.first.wpilibj.command.Subsystem;
+import edu.wpi.first.wpilibj.command.PIDSubsystem;
 import edu.wpi.first.wpilibj.smartdashboard.SmartDashboard;
+import frc.robot.Robot;
 
 /**
  * Subsystem for getting and working with vision data
  * Data is sent from DS, where it is processed, via network tables
  */
-public class VisionSubsystem extends Subsystem {
+public class VisionSubsystem extends PIDSubsystem {
   
   private static NetworkTableInstance inst = NetworkTableInstance.getDefault();
   private static NetworkTable table = inst.getTable("GRIP/contours");
   private double[] defaultValue = new double[0];
   private double[] centerX;
   private double[] centerY;
-  private double[] width;
-  private double[] height;
-  private double[] ratio;
-  private double visionCenter;
+  private double offset;
+
+  public VisionSubsystem() {
+    super("Vision", 0.03, 0.00, 0.00);
+    setAbsoluteTolerance(2);
+    setInputRange(-160, 160);
+    setOutputRange(-1, 1);
+    getPIDController().setContinuous(true);
+  }
+
+  /**
+	 * Method for passing info to the PID controller
+	 * @return distance from ultrasonic in cm
+	 */
+	protected double returnPIDInput() {
+		return offset;
+  }
+  
+  public double getOffset(){
+    return offset;
+  }
+
+  public boolean isEnabled() {
+    return getPIDController().isEnabled();
+  }
+
+	/**
+	 * Method for using the output of the PID controller
+	 * @param output method for driving forward/backwards
+	 * In range of -1 to 1
+	 */
+	protected void usePIDOutput(double output) {
+		Robot.driveExecutor.setX(output * 0.5);
+	}
 
   @Override
   public void initDefaultCommand() {
@@ -40,18 +71,6 @@ public class VisionSubsystem extends Subsystem {
   private void setVars() {
     centerX = table.getEntry("centerX").getDoubleArray(defaultValue);
     centerY = table.getEntry("centerY").getDoubleArray(defaultValue);
-    width = table.getEntry("width").getDoubleArray(defaultValue);
-    height = table.getEntry("height").getDoubleArray(defaultValue);
-
-    ratio = new double[height.length];
-    for(int i = 0; i< centerX.length; i++) {
-      try{
-        ratio[i] = height[i] / width[i];
-      } catch(Exception e) {
-        System.out.println("Something went from when getting ratio data: ");
-        e.printStackTrace();
-      }
-    }
   }
 
 /**
@@ -64,9 +83,7 @@ public class VisionSubsystem extends Subsystem {
     //check for validity of data
     if(centerX.length == 2) {
       SmartDashboard.putBoolean("Vision Ready", true); //puts data into dashboard for driver
-      if(1 < ratio[0] && ratio[0] < 2 && 1 < ratio[1] && ratio[1] < 2) { //check for validity
-        visionCenter = getCenter(centerX);
-      }
+      offset = getVisionOffset(centerX);
     } else {
       System.err.println("Targets not found!"); //debug line
       SmartDashboard.putBoolean("Vision Ready", false); //puts data into dashboard for driver
@@ -74,23 +91,16 @@ public class VisionSubsystem extends Subsystem {
   }
 
 /**
- * Method for getting the current center of vision targets in px
- * @param centerX Array of the centers of contours
- * @return Double value - current X axis center of vision targets
- */
-  private double getCenter(double[] centerX) {
-    double center = (centerX[0] + centerX[1]) / 2;
-    return center;
-  }
-
-/**
  * Method for returning the vision offset from center
+ * @param centerX Array of the centers of contours
+ * @return visionOfset in values from -160 to 160 -> depends on camera resolution
  * Positive = to the right
  * Negative = to the left
- * @return visionOfset in values from -160 to 160 -> depends on camera resolution
  */
-  public double getVisionOffset() {
-    return visionCenter - 160;
+  private double getVisionOffset(double[] centerX) {
+    double center = (centerX[0] + centerX[1]) / 2;
+    return center - 160;
   }
+
 
 }
